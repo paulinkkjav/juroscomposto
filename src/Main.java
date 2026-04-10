a77a4f578a0cfa8e819ed3482ab587be4eede945
paulinkkjav <paulinkk47@gmail.com>
2026-04-10 10:46:16 -0300

    Refactor Main class and implement interest calculation

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
-}
+public class Main {
+
+  public void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+
+      double principal;
+      double taxadejuros;
+      int vezescomposto;
+      int anos;
+      double quantidade;
+
+      System.out.print("Coloque a quantidade em: (R$💵)");
+     principal = sc.nextDouble();
+
+      System.out.print("Coloque a quantidade interessada de taxa de juros (em %): ");
+      taxadejuros = sc.nextDouble() /100;
+
+      System.out.print("Coloque o numero  de vezes composto por ano: ");
+      vezescomposto = sc.nextInt();
+
+      System.out.print("Coloque a quantidade de anos  :");
+      anos = sc.nextInt();
+
+      quantidade = principal * Math.pow(1 + taxadejuros / vezescomposto, vezescomposto * anos);
+      System.out.printf("A quantidade após %d anos foi de %.2f R$💵", anos, quantidade);
+      sc.close();
+    }
